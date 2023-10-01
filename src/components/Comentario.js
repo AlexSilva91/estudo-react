7ff7df5c5725a72f95af4719f02f668458e71225
Alex da Silva Alves <AlexSilvaAlves17@outlook.com>
2023-10-01 14:48:11 -0300

    estilo css

--- src/components/Comentario.js
+++ src/components/Comentario.js
@@ -3,12 +3,14 @@ import './Comentario.css'
 //JSX arrowFuntion
 const Comentario = props => (
     <div className='Comentario'> 
-        <h2 className='nome'>{props.nome}</h2>
-        <div className='msg'>{props.children}</div>
-        <p className='email'>{props.email}</p>
-        <p className='data'>{props.data.toString()}</p>
-        <button className='btnRemover'
+        <div className='conteudo'>
+            <h2 className='nome'>{props.nome}</h2>
+            <div className='msg'>{props.children}</div>
+            <p className='email'>{props.email}</p>
+            <p className='data'>{props.data.toString()}</p>
+            <button className='btnRemover'
             onClick={props.onRemove}>&times;</button>
+        </div>
     </div>
 );
 
